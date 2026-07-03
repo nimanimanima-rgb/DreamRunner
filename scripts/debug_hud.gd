0f560bba62e6682805d1ebe9c1496aa9a4be37d5
Nima <nimaasgari1605@gmail.com>
2026-07-03 23:29:22 +0330

    Fix movement test area layout and colors

--- scripts/debug_hud.gd
+++ scripts/debug_hud.gd
@@ -10,10 +10,19 @@ func _process(_delta: float) -> void:
 	var horizontal_speed := Vector2(player.velocity.x, player.velocity.z).length()
 	var movement_state := "Grounded" if player.is_on_floor() else "Airborne"
 	var sprinting := Input.is_action_pressed("sprint") and horizontal_speed > 0.1
+	var movement_mode := "Airborne"
+	if player.is_on_floor():
+		if horizontal_speed < 0.1:
+			movement_mode = "Idle"
+		elif sprinting:
+			movement_mode = "Sprint"
+		else:
+			movement_mode = "Run"
 
 	readout.text = (
 		"Speed: %5.1f m/s\n" % horizontal_speed
 		+ "State: %s\n" % movement_state
 		+ "Vertical velocity: %5.1f m/s\n" % player.velocity.y
-		+ "Sprinting: %s" % str(sprinting)
+		+ "Sprinting: %s\n" % str(sprinting)
+		+ "Mode: %s" % movement_mode
 	)
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,40 +1,56 @@
 extends CharacterBody3D
 
 @export_group("Movement")
-@export var walk_speed: float = 14.0
-@export var sprint_speed: float = 32.0
-@export var ground_acceleration: float = 70.0
-@export var air_acceleration: float = 22.0
-@export var ground_deceleration: float = 55.0
-@export var air_deceleration: float = 2.0
-@export var jump_velocity: float = 18.0
-@export var gravity_multiplier: float = 2.0
+@export var walk_speed: float = 13.0
+@export var sprint_speed: float = 34.0
+@export var ground_acceleration: float = 58.0
+@export var air_acceleration: float = 10.0
+@export var ground_deceleration: float = 38.0
+@export var air_deceleration: float = 0.5
+@export var jump_velocity: float = 17.5
+@export var gravity_multiplier: float = 1.35
+@export var maximum_fall_speed: float = 42.0
 
 @export_group("Camera")
 @export var mouse_sensitivity: float = 0.003
-@export var pitch_smoothing: float = 14.0
+@export var rotation_smoothing: float = 12.0
 @export var min_pitch: float = -55.0
 @export var max_pitch: float = 25.0
+@export var normal_fov: float = 78.0
+@export var sprint_fov: float = 84.0
+@export var fov_smoothing: float = 5.0
+@export var camera_follow_smoothing: float = 18.0
+@export var camera_vertical_smoothing: float = 7.0
+@export var camera_height: float = 1.4
 
 @onready var camera_pivot: Node3D = $CameraPivot
+@onready var camera: Camera3D = $CameraPivot/SpringArm3D/Camera3D
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var pitch: float = deg_to_rad(-12.0)
 var target_pitch: float = pitch
+var yaw: float = 0.0
+var target_yaw: float = 0.0
 
 
 func _ready() -> void:
 	safe_margin = 0.001
-	floor_snap_length = 0.3
+	floor_snap_length = 0.45
+
+	var initial_camera_position := camera_pivot.global_position
+	camera_pivot.top_level = true
+	camera_pivot.global_position = initial_camera_position
 
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	camera_pivot.rotation.x = pitch
+	yaw = rotation.y
+	target_yaw = yaw
+	camera_pivot.global_rotation = Vector3(pitch, yaw, 0.0)
+	camera.fov = normal_fov
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(-event.relative.x * mouse_sensitivity)
-
+		target_yaw -= event.relative.x * mouse_sensitivity
 		target_pitch -= event.relative.y * mouse_sensitivity
 		target_pitch = clamp(target_pitch, deg_to_rad(min_pitch), deg_to_rad(max_pitch))
 
@@ -46,9 +62,26 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _process(delta: float) -> void:
-	var smoothing_weight := 1.0 - exp(-pitch_smoothing * delta)
+	var smoothing_weight := 1.0 - exp(-rotation_smoothing * delta)
+	yaw = lerp_angle(yaw, target_yaw, smoothing_weight)
 	pitch = lerp_angle(pitch, target_pitch, smoothing_weight)
-	camera_pivot.rotation.x = pitch
+	rotation.y = yaw
+	camera_pivot.global_rotation = Vector3(pitch, yaw, 0.0)
+
+	var camera_target := global_position + Vector3.UP * camera_height
+	var follow_weight := 1.0 - exp(-camera_follow_smoothing * delta)
+	var vertical_weight := 1.0 - exp(-camera_vertical_smoothing * delta)
+	var camera_position := camera_pivot.global_position
+	camera_position.x = lerpf(camera_position.x, camera_target.x, follow_weight)
+	camera_position.z = lerpf(camera_position.z, camera_target.z, follow_weight)
+	camera_position.y = lerpf(camera_position.y, camera_target.y, vertical_weight)
+	camera_pivot.global_position = camera_position
+
+	var horizontal_speed := Vector2(velocity.x, velocity.z).length()
+	var is_sprinting := Input.is_action_pressed("sprint") and horizontal_speed > walk_speed
+	var target_fov := sprint_fov if is_sprinting else normal_fov
+	var fov_weight := 1.0 - exp(-fov_smoothing * delta)
+	camera.fov = lerpf(camera.fov, target_fov, fov_weight)
 
 
 func _physics_process(delta: float) -> void:
@@ -73,7 +106,10 @@ func _physics_process(delta: float) -> void:
 	velocity.z = horizontal_velocity.z
 
 	if not is_on_floor():
-		velocity.y -= gravity * gravity_multiplier * delta
+		velocity.y = maxf(
+			velocity.y - gravity * gravity_multiplier * delta,
+			-maximum_fall_speed
+		)
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
